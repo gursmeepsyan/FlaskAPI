b4ac0b380211d56b4136ba74d9c618314da1bb53
Gursmeep Syan <gursmeep@gmail.com>
2023-10-08 05:56:32 +0000

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-// 
+from flask import Flask 
+from flask_restful import Resource , Api
+
+app = Flask("VideoAPI")
+api = Api(app)
+
+videos = {
+    'video1': { 'title': "I am 1"},
+    'video2': { 'title': "I am 2"}
+
+}
+
+class Video(Resource):
+
+    def get(self):
+        return videos
+
+api.add_resource(Video,'/') 
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
